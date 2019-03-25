c6df28f5d3d1a214d20b662c9287e8da42cbba7d
Simon Li <spli@dundee.ac.uk>
2019-03-25 16:16:59 +0000

    Change logging to debug, use __name__ for logger

--- src/omero_upload/library.py
+++ src/omero_upload/library.py
@@ -5,8 +5,7 @@ from StringIO import StringIO
 import omero.clients
 from omero.gateway import BlitzGateway
 
-log = logging.getLogger(__file__)
-# log.setLevel(logging.DEBUG)
+log = logging.getLogger(__name__)
 
 bufsize = 1048576
 
@@ -30,20 +29,21 @@ def upload_ln_s(client, filepath, omero_data_dir, mimetype=None):
 
     conn = BlitzGateway(client_obj=client)
 
-    log.info('Creating empty OriginalFile placeholder')
+    log.debug('Creating empty OriginalFile placeholder')
     placeholder = StringIO('')
     fo = conn.createOriginalFileFromFileObj(
         placeholder, filedir, filename, 0, mimetype=mimetype)
     omero_path = os.path.join(
         omero_data_dir, 'Files', omero.util.long_to_path(fo.id))
 
-    log.info('OriginalFile:%d Deleting %s', fo.id, omero_path)
+    log.debug('OriginalFile:%d Deleting %s', fo.id, omero_path)
     os.remove(omero_path)
 
-    log.info('OriginalFile:%d Symlinking %s to %s', fo.id, abspath, omero_path)
+    log.debug('OriginalFile:%d Symlinking %s to %s',
+              fo.id, abspath, omero_path)
     os.symlink(abspath, omero_path)
 
-    log.info('OriginalFile:%d Getting size and checksum', fo.id)
+    log.debug('OriginalFile:%d Getting size and checksum', fo.id)
     size = os.path.getsize(abspath)
     h = sha1()
     with open(abspath) as f:
@@ -54,8 +54,8 @@ def upload_ln_s(client, filepath, omero_data_dir, mimetype=None):
             h.update(data)
     hash = h.hexdigest()
 
-    log.info('OriginalFile:%d Saving size:%d and checksum:%s',
-             fo.id, size, hash)
+    log.debug('OriginalFile:%d Saving size:%d and checksum:%s',
+              fo.id, size, hash)
     fo.setSize(omero.rtypes.rlong(size))
     fo.setHash(omero.rtypes.rstring(hash))
     chk = omero.model.ChecksumAlgorithmI()
