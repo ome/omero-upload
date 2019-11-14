4ed6bf7e11291536b07428cc0abacbeb89af6665
Sebastien Besson <seb.besson@gmail.com>
2019-11-14 22:20:19 +0000

    Futurize the omero-upload library

--- src/omero_upload/__init__.py
+++ src/omero_upload/__init__.py
@@ -17,7 +17,8 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-from library import upload_ln_s
+from __future__ import absolute_import
+from .library import upload_ln_s
 __all__ = (
     'upload_ln_s',
 )
--- src/omero_upload/library.py
+++ src/omero_upload/library.py
@@ -17,10 +17,13 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+from future import standard_library
+standard_library.install_aliases() # noqa
+from builtins import str
 from hashlib import sha1
 import logging
 import os
-from StringIO import StringIO
+from io import StringIO
 from uuid import uuid4
 import omero.clients
 from omero.gateway import BlitzGateway
